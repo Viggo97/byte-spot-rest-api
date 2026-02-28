bace67977c389b8f38c5c56031fb069553e8705a
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-02-28 09:11:20 +0100

    feat(value-object): Add additional constructor to Salary

--- src/ByteSpot.Domain/ValueObjects/Offer/Salary.cs
+++ src/ByteSpot.Domain/ValueObjects/Offer/Salary.cs
@@ -7,12 +7,14 @@ public sealed record Salary
     public int? Min { get; }
     public int? Max { get; }
     public int? Fixed { get; }
+    
+    private Salary() {}
 
     public Salary(int value)
     {
         Fixed = value;
     }
-
+    
     public Salary(int minValue, int maxValue)
     {
         if (minValue > maxValue)
