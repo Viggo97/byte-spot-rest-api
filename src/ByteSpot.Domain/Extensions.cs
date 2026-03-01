ed56e480002bf3d2d70c1e429fb752b57aec904f
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-03-01 13:30:14 +0100

    feat(domain): Add service registation method

---
+++ src/ByteSpot.Domain/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ByteSpot.Domain;
+
+public static class Extensions
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        return services;
+    }
+}
