d9a11c8cbf7a60b513ed4488d40fe2dfe13f3cd4
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-03-01 13:30:44 +0100

    feat(app): Startup setup

--- src/ByteSpot.Api/Program.cs
+++ src/ByteSpot.Api/Program.cs
@@ -1,23 +1,34 @@
 using ByteSpot.Api.Endpoints;
+using ByteSpot.Application;
+using ByteSpot.Domain;
+using ByteSpot.Infrastructure;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services
+    .AddDomain()
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration)
+    .AddOpenApi();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference(options =>
-    {
-        options.WithTitle("Byte Spot Api");
-    });
+    app.MapScalarApiReference(options => { options.WithTitle("Byte Spot Api"); });
 }
 
 app.UseHttpsRedirection();
 
-app.MapOfferEndpoints();
+app
+    .MapOfferEndpoints()
+    .MapCompanyEndpoints()
+    .MapLocationEndpoints()
+    .MapTechnologyEndpoints()
+    .MapWorkModeEndpoints()
+    .MapExperienceLevelEndpoints()
+    .MapEmploymentTypeEndpoints();
 
 app.Run();
