dc198f70430a44949ff216f88aae1aa3b108fea9
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-03-10 19:33:31 +0100

    feat(api): Add language code header converter

---
+++ src/ByteSpot.Api/Utils/LanguageCodeConverter.cs
@@ -0,0 +1,14 @@
+﻿using ByteSpot.Domain.Enums;
+
+namespace ByteSpot.Api.Utils;
+
+public static class LanguageCodeConverter
+{
+    public static LanguageCode Get(HttpContext httpContext)
+    {
+        var acceptLanguageHeader = httpContext.Request.Headers.AcceptLanguage;
+        var languageParsed = Enum.TryParse(acceptLanguageHeader, ignoreCase: true, out LanguageCode languageCode);
+
+        return languageParsed ? languageCode : LanguageCode.En;
+    }
+}
