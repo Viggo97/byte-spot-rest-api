b4971d462c2ea4c11bfc52aeecfbc760b759a89d
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-03-16 20:38:14 +0100

    refactor(location): Add ordering, remove repository layer

---
+++ src/ByteSpot.Infrastructure/DAL/Handlers/GetLocationsHandler.cs
@@ -0,0 +1,19 @@
+﻿using ByteSpot.Application.Abstractions;
+using ByteSpot.Application.Dto;
+using ByteSpot.Application.Queries;
+using ByteSpot.Infrastructure.DAL.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ByteSpot.Infrastructure.DAL.Handlers;
+
+internal sealed class GetLocationsHandler(ByteSpotDbContext dbContext) 
+    : IQueryHandler<GetLocationsQuery, IEnumerable<LocationDto>>
+{
+    public async Task<IEnumerable<LocationDto>> HandleAsync(GetLocationsQuery query)
+    {
+        return await dbContext.Locations
+            .OrderBy(location => location.Name)
+            .Select(location => new LocationDto(location.Id, location.Name))
+            .ToListAsync();
+    }
+}
