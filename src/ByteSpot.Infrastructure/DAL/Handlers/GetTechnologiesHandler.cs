b0f6233d1f7543c2d0eb43ba2821985f0cff3ef1
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-03-16 20:38:24 +0100

    refactor(technology): Add ordering, remove repository layer

---
+++ src/ByteSpot.Infrastructure/DAL/Handlers/GetTechnologiesHandler.cs
@@ -0,0 +1,21 @@
+﻿using ByteSpot.Application.Abstractions;
+using ByteSpot.Application.Dto;
+using ByteSpot.Application.Queries;
+using ByteSpot.Infrastructure.DAL.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ByteSpot.Infrastructure.DAL.Handlers;
+
+internal sealed class GetTechnologiesHandler(ByteSpotDbContext dbContext)
+    : IQueryHandler<GetTechnologiesQuery, IEnumerable<TechnologyDto>>
+{
+    public async Task<IEnumerable<TechnologyDto>> HandleAsync(GetTechnologiesQuery query)
+    {
+        return await dbContext.Technologies
+            .OrderBy(technology => technology.Name)
+            .Select(technology =>
+                new TechnologyDto(technology.Id, technology.Name, technology.IconCode)
+            )
+            .ToListAsync();
+    }
+}
