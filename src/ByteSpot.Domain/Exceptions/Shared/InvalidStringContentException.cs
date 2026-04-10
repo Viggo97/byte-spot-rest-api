68095ac2991a6eb2669cf67c1d6b0b6f6a100cca
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-04-10 19:57:58 +0200

    chore(exceptions): Extend InvalidStringContent

--- src/ByteSpot.Domain/Exceptions/Shared/InvalidStringContentException.cs
+++ src/ByteSpot.Domain/Exceptions/Shared/InvalidStringContentException.cs
@@ -1,3 +1,4 @@
 ﻿namespace ByteSpot.Domain.Exceptions.Shared;
 
-public class InvalidStringContentException() : CustomException("String cannot contain whitespaces.");
+public class InvalidStringContentException(string? objName = null) 
+    : CustomException($"String cannot contain whitespaces or be null{(objName is not null ? $" for: {objName}." : ".")}");
