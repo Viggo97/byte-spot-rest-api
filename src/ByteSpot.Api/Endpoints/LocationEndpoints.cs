d52ffca3b29e7ece434077b2cc23c6f99467a925
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-04-30 12:27:26 +0200

    fix(location): Fix name

--- src/ByteSpot.Api/Endpoints/LocationEndpoints.cs
+++ src/ByteSpot.Api/Endpoints/LocationEndpoints.cs
@@ -1,7 +1,6 @@
 ﻿using ByteSpot.Application.Abstractions;
 using ByteSpot.Application.Commands.Location;
 using ByteSpot.Application.Dto;
-using ByteSpot.Application.Queries;
 using ByteSpot.Application.Queries.Location;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,32 +12,32 @@ public static class LocationEndpoints
     
     public static WebApplication MapLocationEndpoints(this WebApplication app)
     {
-        var users = app.MapGroup(Route).WithTags("Locations");
+        var locationsGroup = app.MapGroup(Route).WithTags("Locations");
         
-        users.MapGet("", async (IQueryHandler<GetLocationsQuery, IEnumerable<LocationDto>> handler) =>
+        locationsGroup.MapGet("", async (IQueryHandler<GetLocationsQuery, IEnumerable<LocationDto>> handler) =>
         {
             var locations = await handler.HandleAsync(new GetLocationsQuery());
             return Results.Ok(locations);
         });
 
-        users.MapGet("{id:guid}", async (Guid id, IQueryHandler<GetLocationByIdQuery, LocationDto> handler) =>
+        locationsGroup.MapGet("{id:guid}", async (Guid id, IQueryHandler<GetLocationByIdQuery, LocationDto> handler) =>
         {
             var location = await handler.HandleAsync(new GetLocationByIdQuery(id));
             return Results.Ok(location);
         });
         
-        users.MapGet("{name}", async (string name, IQueryHandler<GetLocationByNameQuery, LocationDto> handler) =>
+        locationsGroup.MapGet("{name}", async (string name, IQueryHandler<GetLocationByNameQuery, LocationDto> handler) =>
         {
             var location = await handler.HandleAsync(new GetLocationByNameQuery(name));
             return Results.Ok(location);
         });
 
-        users.MapPost("", async (AddLocationCommand command, ICommandHandler<AddLocationCommand> handler) =>
+        locationsGroup.MapPost("", async (AddLocationCommand command, ICommandHandler<AddLocationCommand> handler) =>
         {
             await handler.HandleAsync(command);
         });
         
-        users.MapPut("{id:guid}", async (Guid id, [FromBody] UpdateLocationCommand command,
+        locationsGroup.MapPut("{id:guid}", async (Guid id, [FromBody] UpdateLocationCommand command,
             ICommandHandler<UpdateLocationCommand> handler) =>
         {
             var tmp = command with { Id = id };
@@ -47,7 +46,7 @@ public static class LocationEndpoints
             return Results.NoContent();
         });
         
-        users.MapDelete("{id:guid}", async (Guid id, ICommandHandler<RemoveLocationCommand> handler) =>
+        locationsGroup.MapDelete("{id:guid}", async (Guid id, ICommandHandler<RemoveLocationCommand> handler) =>
         {
             await handler.HandleAsync(new RemoveLocationCommand(id));
             
