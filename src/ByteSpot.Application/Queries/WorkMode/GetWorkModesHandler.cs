02e85d83b3cff69a3582b7f656c487fe8195cfc6
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-05-01 14:56:01 +0200

    feat(work-mode): Move handler

---
+++ src/ByteSpot.Application/Queries/WorkMode/GetWorkModesHandler.cs
@@ -0,0 +1,28 @@
+﻿using ByteSpot.Application.Abstractions;
+using ByteSpot.Application.Dto;
+using ByteSpot.Domain.Repositories;
+
+namespace ByteSpot.Application.Queries.WorkMode;
+
+internal sealed class GetWorkModesHandler
+    : IQueryHandler<GetWorkModesQuery, IEnumerable<WorkModeDto>>
+{
+    private readonly IWorkModeRepository _workModeRepository;
+
+    public GetWorkModesHandler(IWorkModeRepository workModeRepository)
+    {
+        _workModeRepository = workModeRepository;
+    }
+    
+    public async Task<IEnumerable<WorkModeDto>> HandleAsync(GetWorkModesQuery query)
+    {
+        var languageCode = query.LanguageCode;
+        var workModes = await _workModeRepository.GetAllAsync();
+
+        return workModes.Select(workMode => new WorkModeDto(
+            workMode.Id,
+            workMode.Translations.Single(translation => translation.LanguageCode == languageCode).Name
+            )
+        );
+    }
+}
