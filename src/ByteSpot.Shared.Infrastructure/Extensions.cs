61246a1c078451668c671214ad9694ef8159ed57
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-06-25 11:10:19 +0200

    chore(app): Add shared modules

---
+++ src/ByteSpot.Shared.Infrastructure/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+[assembly: InternalsVisibleTo("ByteSpot.Bootstrapper")]
+namespace ByteSpot.Shared.Infrastructure;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        return app;
+    }
+}
